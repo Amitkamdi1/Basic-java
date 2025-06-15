e5b13a062ba333ccbe9baf3ece1d780aee40b51f
Amitk <amitkamdi12@gmail.com>
2025-06-15 11:38:55 +0530

    3rd repository file

---
+++ If_block_02.java
@@ -0,0 +1,40 @@
+package variables;
+
+public class If_block_02 {
+
+	public static void main(String[] args) {
+		
+		int num =10;
+		int num1= -20;
+		int num22 = 0;
+		if (num> 0){
+			
+			System.out.println("Postive number");
+			
+			}
+
+		if (10>num1) {
+			
+			System.out.println("negative number");
+		}
+		
+		
+		if (num22==0) {
+			
+			
+			System.out.println("zero number");
+		}
+		
+		int a = 15, b = 10; // Change values to test different cases
+
+        if (a > b) {
+            System.out.println(a + " is greater than " + b);
+        }
+        
+        if (b > a) {
+            System.out.println(b + " is greater than " + a);
+        }
+		
+	}
+
+}
